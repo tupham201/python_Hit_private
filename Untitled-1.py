66087ce821c223e3d1879ebb04e35ef46c8dd7b1
mihow201 <phamtattu201@gmail.com>
2024-10-27 14:30:26 +0700

    bai1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,22 @@
-print("hello word")
+a = 5
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+if a > b:
+    print("a lon hon b")
+if a < b:
+    print("a nho hon b")
+if a == b:
+    print("a bang b")
+
+print(a & b)
+print(a | b)
+print(not a == b)
+print(a ^ b)
+print(a << 5)
+print(a >> 6)
+print(~a)
---
+++ bttuan2/bai1.py
@@ -0,0 +1,22 @@
+a = 5
+b = 5
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a x b = ",a*b)
+print("a / b = ",a/b)
+print("a mũ b = ",a**b)
+print("a chia cho b dư = ",a%b)
+if a > b:
+    print("a lớn hơn b")
+if a < b:
+    print("a nhỏ hơn b")
+if a == b:
+    print("a bằng b")
+
+print(a & b)
+print(a | b)
+print(not a == b)
+print(a ^ b)
+print(a << 5)
+print(a >> 6)
+print(~a)
